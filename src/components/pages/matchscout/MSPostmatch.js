44c69a7618fa5f1506167a41a40b98fdeeeda299
ericoj100 <144740721+ericoj100@users.noreply.github.com>
2025-01-11 12:19:44 -0800

    pafpoa
    poopp

--- src/components/pages/matchscout/MSPostmatch.js
+++ src/components/pages/matchscout/MSPostmatch.js
@@ -1,8 +1,8 @@
 import Grid2 from "@mui/material/Unstable_Grid2";
-import {useState} from "react";
-import {MatchStage} from "../../MatchConstants";
+import { useState } from "react";
+import { MatchStage } from "../../MatchConstants";
 import CustomInput from "./form_elements/CustomInput";
-import {Box, Typography, Stack} from "@mui/material";
+import { Box, Typography, Stack } from "@mui/material";
 import CustomToggleButton from "./form_elements/CustomToggleButton";
 
 export default function MSPostmatch(props) {
@@ -48,23 +48,158 @@ export default function MSPostmatch(props) {
                     }}
                 />
                 <Typography variant={"h6"}>
-                        Check All That Apply
-                    </Typography>
-                <Stack direction={"row"} spacing={0.5}> 
-                    {/* <CustomToggleButton
-                        label={"Intake Broken"}
-                        value={data.getPostMData("intakeBroken")}
-                        onClick={(newValue) => {
-                            data.setPostMData("intakeBroken", newValue);
-                            update();
-                    }}
-                    showCheckbox={false}
-                    /> */}
-                </Stack>
+                    Check All That Apply
+                </Typography>
+
+                {/* Wrap CustomToggleButtons in Grid2 */}
+                <Grid2 container spacing={2}>
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Intake Broken"}
+                            value={data.getPostData("intakeBroken")}
+                            onClick={(newValue) => {
+                                data.setPostData("intakeBroken", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Outtake Broken"}
+                            value={data.getPostData("outtakeBroken")}
+                            onClick={(newValue) => {
+                                data.setPostData("outtakeBroken", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Elevator Broken"}
+                            value={data.getPostData("elevatorBroken")}
+                            onClick={(newValue) => {
+                                data.setPostData("elevatorBroken", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Arm Broken"}
+                            value={data.getPostData("armBroken")}
+                            onClick={(newValue) => {
+                                data.setPostData("armBroken", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Consistently Browns Out"}
+                            value={data.getPostData("brownsOut")}
+                            onClick={(newValue) => {
+                                data.setPostData("brownsOut", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Tips Over / Very Wobbly"}
+                            value={data.getPostData("wobbly")}
+                            onClick={(newValue) => {
+                                data.setPostData("wobbly", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Consistently Misses Outtakes"}
+                            value={data.getPostData("missesOuttakesConsistently")}
+                            onClick={(newValue) => {
+                                data.setPostData("missesOuttakesConsistently", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Slow Intakes"}
+                            value={data.getPostData("slowIntakes")}
+                            onClick={(newValue) => {
+                                data.setPostData("slowIntakes", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Was Disabled"}
+                            value={data.getPostData("disabled")}
+                            onClick={(newValue) => {
+                                data.setPostData("disabled", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Good Defense From Opponents"}
+                            value={data.getPostData("goodDefenseFromOpponents")}
+                            onClick={(newValue) => {
+                                data.setPostData("goodDefenseFromOpponents", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Played Defense"}
+                            value={data.getPostData("playedMajorityDefense")}
+                            onClick={(newValue) => {
+                                data.setPostData("playedMajorityDefense", newValue);
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+
+                    {/* Add the new CustomToggleButton */}
+                    <Grid2 xs={6}>
+                        <CustomToggleButton
+                            label={"Touch It Own It Intake"}
+                            value={data.getPostData("touchItOwnIt")}
+                            onClick={(newValue) => {
+                                data.setPostData("touchItOwnIt", newValue); // Fixed syntax error here
+                                update();
+                            }}
+                            showCheckbox={false}
+                        />
+                    </Grid2>
+                </Grid2>
             </Grid2>
-            <Box sx={{
-                mt: 3,
-            }}/>
+            <Box sx={{ mt: 3 }} />
         </>
     );
 }
