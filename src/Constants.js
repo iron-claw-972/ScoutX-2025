619b7ec2653df82244783eaf249c21b05ce0a21f
ericoj100 <144740721+ericoj100@users.noreply.github.com>
2025-02-23 11:48:54 -0800

    dwqdwq

--- src/Constants.js
+++ src/Constants.js
@@ -65,18 +65,18 @@ export const Constants = {
     developers: [
         {
             name: "Eric Jacobson",
-            year: "2nd Year",
-            icon: require("./assets/jacob.jpeg"),
+            year: "Scouting App Lead - 2nd Year",
+            icon: require("./assets/field.png"),
         },
         {
             name: "Maxwell Tan",
-            year: "1st Year",
-            icon: require("./assets/jacob.jpeg"),
+            year: "Developer - 1st Year",
+            icon: require("./assets/field.png"),
         },
         {
             name: "William Han",
-            year: "2nd Year",
-            icon: require("./assets/jacob.jpeg"),
+            year: "Developer - 2nd Year",
+            icon: require("./assets/field.png"),
         },
     ],
 
